8cbb8311578c13159043adb0413739d5a3930634
James Reed <reedandj1@gmail.com>
2022-02-11 17:30:52 +0000

    add logic to login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,23 @@ import React, { useState, useContext } from "react";
 import styles from "../styles/Login.module.css";
 import { UserContext } from "../contexts/User";
 import { getUsers } from "../utils/api";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const { loggedInUser, setLoggedInUser } = useContext(UserContext);
   const [username, setUsername] = useState("");
+  const navigate = useNavigate();
   function handleSubmit(event) {
     event.preventDefault();
-    getUsers().then(({ users }) => {
-      console.log(users);
-    });
+    getUsers()
+      .then(({ users }) => {
+        const found = users.find((user) => user.username === username);
+        setLoggedInUser(found);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   function handleChange(event) {
     setUsername(event.target.value);
