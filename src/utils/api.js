6f6e9ea65239e77ba497734618e16bcbfc760e50
reedanj <james.r@unitaskr.com>
2023-07-25 19:21:00 +0100

    fix url for backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const articlesApi = axios.create({
-  baseURL: "https://james-news-app.herokuapp.com/api",
+  baseURL: "https://be-nc-news-diz4.onrender.com/api",
 });
 
 export const getArticles = (slug, sort_by, order) => {
@@ -25,11 +25,9 @@ export const getSingleArticle = (article_id) => {
 };
 
 export const getComments = (article_id) => {
-  return articlesApi
-    .get(`/articles/${article_id}/comments`)
-    .then(({ data }) => {
-      return data;
-    });
+  return articlesApi.get(`/articles/${article_id}/comments`).then(({ data }) => {
+    return data;
+  });
 };
 
 export const addVote = (article_id) => {
